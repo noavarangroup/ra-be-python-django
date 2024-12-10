f51a0ee915507fba83a3d1db1f9658c60eb35228
Shahab Siampour <shahab@siampour.ir>
2024-12-10 10:15:57 +0330

    add export to excel

--- vouchers/views.py
+++ vouchers/views.py
@@ -1,8 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from django.http import HttpResponse
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
+
+import pandas as pd
 
 from .models import Master, Subsidiary, Person
 from .serializers import MasterSerializer, SubsidiarySerializer, PersonSerializer
@@ -32,6 +36,17 @@ class MasterViewSet(SoftDeleteMixin, ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['code', 'title']
 
+    @action(detail=False, methods=['get'])
+    def export(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        data = MasterSerializer(queryset, many=True).data
+        df = pd.DataFrame(data)
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=masters.xlsx'
+        df.to_excel(response, index=False)
+        return response
+
 
 class SubsidiaryViewSet(SoftDeleteMixin, ModelViewSet):
     queryset = Subsidiary.objects.all()
@@ -39,3 +54,13 @@ class SubsidiaryViewSet(SoftDeleteMixin, ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['code', 'title']
 
+    @action(detail=False, methods=['get'])
+    def export(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        data = MasterSerializer(queryset, many=True).data
+        df = pd.DataFrame(data)
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=masters.xlsx'
+        df.to_excel(response, index=False)
+        return response
