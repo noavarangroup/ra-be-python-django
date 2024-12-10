5a30a7b305a858331c3ecb9564528a89a057fb41
Shahab Siampour <shahab@siampour.ir>
2024-12-10 10:09:23 +0330

    add validation to Subsidiary

--- vouchers/serializers.py
+++ vouchers/serializers.py
@@ -19,3 +19,11 @@ class SubsidiarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Subsidiary
         fields = '__all__'
+
+    def validate(self, data):
+        parent = data.get('parent')
+        master = data.get('master')
+        if parent:
+            if parent.master!= master:
+                raise serializers.ValidationError('The master of the Subsidiary and its parent must be the same.')
+        return data
