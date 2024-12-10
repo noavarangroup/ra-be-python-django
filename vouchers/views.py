007134ec2d93369533f04b5e124e7825bfa2ac00
Shahab Siampour <shahab@siampour.ir>
2024-12-10 10:08:58 +0330

    add urls and viewsets

--- vouchers/views.py
+++ vouchers/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Master, Subsidiary, Person
+from .serializers import MasterSerializer, SubsidiarySerializer, PersonSerializer
+
 
 # Create your views here.
+
+class SoftDeleteMixin:
+    """This is for soft delete """
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_deleted = True
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PersonViewSet(ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    filter_backends = [DjangoFilterBackend]
+
+
+class MasterViewSet(SoftDeleteMixin, ModelViewSet):
+    queryset = Master.objects.all()
+    serializer_class = MasterSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['code', 'title']
+
+
+class SubsidiaryViewSet(SoftDeleteMixin, ModelViewSet):
+    queryset = Subsidiary.objects.all()
+    serializer_class = SubsidiarySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['code', 'title']
+
